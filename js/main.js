539b5dfe165d94e0999d80882cfbcd262184994f
ZenCarriere <59712949+ZenCarriere@users.noreply.github.com>
2020-11-04 22:01:32 -0500

    images

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@ import { fetchData } from "./modules/DataMiner.js";
 
 (() => {
 
-
-    function ErrorMessageBox(message) {
-        alert("An error has occured");
-    }
-
-
     function handleDataSet (data) {
         let faveSection = document.querySelector('.fave-section'),
         faveTemplate = document.querySelector('#fave-template').content;
@@ -16,10 +10,10 @@ import { fetchData } from "./modules/DataMiner.js";
             let currentFave = faveTemplate.cloneNode(true),
             currentFaveText = currentFave.querySelector('.fave').children;
 
-            currentFaveText[0].src = `./images/${data[fave].image}`;
             currentFaveText[1].textContent = data[fave].name;
-            currentFaveText[2].textContent = data[fave].description;
-            currentFaveText[3].textContent = data[fave].start;
+            currentFaveText[2].src = `images/${data[fave].image}`;
+            currentFaveText[3].textContent = data[fave].description;
+            currentFaveText[4].textContent = data[fave].start;
 
             faveSection.appendChild(currentFave);
         }
